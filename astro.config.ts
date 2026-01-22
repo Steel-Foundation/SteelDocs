745665d8f3d0fec92a9030b78206f381610b15d3
4lve <alve.jeansson@gmail.com>
2026-01-22 21:08:30 +0100

    Add site url

--- astro.config.ts
+++ astro.config.ts
@@ -3,6 +3,7 @@ import starlight from "@astrojs/starlight";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://steel-foundation.github.io/SteelDocs/",
   integrations: [
     starlight({
       title: "My Docs",
