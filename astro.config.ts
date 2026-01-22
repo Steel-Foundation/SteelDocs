0d04d827882a884bad8834aabc9755b3b6431c21
4lve <alve.jeansson@gmail.com>
2026-01-22 21:13:20 +0100

    fix css

--- astro.config.ts
+++ astro.config.ts
@@ -4,6 +4,7 @@ import starlight from "@astrojs/starlight";
 // https://astro.build/config
 export default defineConfig({
   site: "https://steel-foundation.github.io/SteelDocs/",
+  base: "/SteelDocs/",
   integrations: [
     starlight({
       title: "My Docs",
