9223de7afd5683041d2094f6fe7cede8fa12f86f
Wouf <fuckoff@example.comm>
2026-02-19 04:04:07 +0100

    do not merde pls

---
+++ src/components/homepage/header.tsx
@@ -0,0 +1,79 @@
+import { ArrowDown, CookingPot } from 'lucide-react';
+
+const glassStyle = {
+  backdropFilter: 'blur(10px)',
+  WebkitBackdropFilter: 'blur(10px)',
+  boxShadow: '0 4px 30px rgba(0, 0, 0, 0.1)',
+};
+
+const Pill = ({ text }: { text: string }) => (
+  <div
+    className="text-white w-auto px-4 h-8.5 flex justify-center items-center rounded-[50px] font-medium text-xs md:text-sm bg-white/5 border border-white/10"
+    style={glassStyle}
+  >
+    <CookingPot className="w-4 h-4" />
+    <span className="ml-1">{text}</span>
+  </div>
+);
+
+const Headline = ({ subtitle }: { subtitle: string }) => (
+  <>
+    <h1
+      className="mt-4 text-white text-center leading-[1.2] -tracking-[2px] max-w-[16ch] font-minecraft text-7xl"
+    >
+      Steel<span className="text-emerald-500 font-bold italic">MC</span>
+    </h1>
+
+    <h2 className='text-white max-w-150 text-center'>
+      {subtitle}
+    </h2>
+  </>
+);
+
+const Actions = ({ mainCtaText, secondCtaText }: { mainCtaText: string; secondCtaText: string }) => (
+  <div className="flex gap-4 mt-8 items-center pointer-events-auto">
+    {/* Main CTA */}
+    <button
+      className="px-6 md:px-8 py-2 font-minecraft font-bold bg-white text-black rounded-[50px] text-xs md:text-sm border-none cursor-pointer hover:bg-gray-100 hover:-translate-y-px transition-all duration-200 ease-out"
+    >
+      {mainCtaText}
+    </button>
+
+    {/* Second CTA */}
+    <button
+      className="px-6 md:px-8 py-2 font-minecraft font-bold rounded-[50px] text-xs md:text-sm bg-white/5 border border-white/10 text-white/85 cursor-pointer hover:bg-white/10 hover:-translate-y-px transition-all duration-200 ease-out"
+      style={glassStyle}
+    >
+      {secondCtaText}
+    </button>
+  </div>
+);
+
+interface HeaderProps {
+  pillText: string;
+  subtitle: string;
+  mainCtaText: string;
+  secondCtaText: string;
+}
+
+const BackgroundContent = ({
+  pillText,
+  subtitle,
+  mainCtaText,
+  secondCtaText
+}: HeaderProps) => {
+
+  return (
+    <div className="select-none relative w-full h-full overflow-hidden">
+
+      <div className="absolute top-0 left-0 flex flex-col items-center justify-center w-full h-full z-1 pointer-events-none">
+        <Pill text={pillText} />
+        <Headline subtitle={subtitle} />
+        <Actions mainCtaText={mainCtaText} secondCtaText={secondCtaText} />
+        <ArrowDown className="w-10 h-10 p-2 mt-16 text-white animate-bounce  bg-white/30 rounded-full" />
+      </div>
+    </div>
+  );
+};
+
+export default BackgroundContent;
