1bf71f318b582d903f2f9ae5c37cc399020bbfec
4lve <alve.jeansson@gmail.com>
2026-06-14 22:38:19 +0200

    Fix config

--- astro.config.ts
+++ astro.config.ts
@@ -5,125 +5,128 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://steel-foundation.github.io/SteelDocs/",
-  base: "/SteelDocs/",
+  site: "https://steelmc.dev/",
+  base: "/",
   output: "static",
 
-  integrations: [starlight({
-    customCss: ["./src/styles/global.css"],
-    favicon: "/favicon.png",
-    title: {
-      en: "Steel-Docs",
-      de: "Steel-Doku",
-      es: "Documentacion de Steel",
-    },
-    social: [
-      {
-        icon: "discord",
-        label: "Discord",
-        href: "https://discord.gg/MwChEHnAbh",
+  integrations: [
+    starlight({
+      customCss: ["./src/styles/global.css"],
+      favicon: "/favicon.png",
+      title: {
+        en: "Steel-Docs",
+        de: "Steel-Doku",
+        es: "Documentacion de Steel",
       },
-      {
-        icon: "github",
-        label: "GitHub",
-        href: "https://github.com/Steel-Foundation/SteelMC",
-      },
-    ],
-    sidebar: [
-      {
-        label: "Getting started",
-        translations: {
-          es: "Primeros pasos",
-          de: "Erste Schritte",
+      social: [
+        {
+          icon: "discord",
+          label: "Discord",
+          href: "https://discord.gg/MwChEHnAbh",
         },
-        autogenerate: { directory: "guides/getting-started" },
-      },
-      {
-        label: "Configuration",
-        translations: {
-          es: "Configuración",
-          de: "Konfiguration",
+        {
+          icon: "github",
+          label: "GitHub",
+          href: "https://github.com/Steel-Foundation/SteelMC",
         },
-        autogenerate: { directory: "guides/configuration" },
-      },
-      {
-        label: "Development",
-        translations: {
-          es: "Desarrollo",
-          de: "Entwicklung",
+      ],
+      sidebar: [
+        {
+          label: "Getting started",
+          translations: {
+            es: "Primeros pasos",
+            de: "Erste Schritte",
+          },
+          autogenerate: { directory: "guides/getting-started" },
         },
-        items: [
-          "guides/development/decompile-minecraft",
-          "guides/development/upgrade-minecraft",
-          "guides/development/code-standard",
-          "guides/development/block_item_registration",
-          "guides/development/registries",
-          {
-            label: "Blocks",
-            translations: {
-              es: "Bloques",
-              de: "Blöcke",
-            },
-            autogenerate: { directory: "guides/development/blocks" },
+        {
+          label: "Configuration",
+          translations: {
+            es: "Configuración",
+            de: "Konfiguration",
           },
-          {
-            label: "Items",
-            translations: {
-              es: "Objetos",
-              de: "Items",
-            },
-            autogenerate: { directory: "guides/development/items" },
-          },
-          {
-            label: "Network",
-            translations: {
-              es: "Red",
-              de: "Netzwerk",
-            },
-            autogenerate: { directory: "guides/development/network" },
-          },
-          {
-            label: "Tools",
-            translations: {
-              es: "Herramientas",
-              de: "Werkzeuge",
-            },
-            autogenerate: { directory: "guides/development/tools" },
-          },
-        ],
-      },
-      {
-        label: "Reference",
-        translations: {
-          es: "Referencias",
-          de: "Referenz",
+          autogenerate: { directory: "guides/configuration" },
         },
-        autogenerate: { directory: "reference" },
+        {
+          label: "Development",
+          translations: {
+            es: "Desarrollo",
+            de: "Entwicklung",
+          },
+          items: [
+            "guides/development/decompile-minecraft",
+            "guides/development/upgrade-minecraft",
+            "guides/development/code-standard",
+            "guides/development/block_item_registration",
+            "guides/development/registries",
+            {
+              label: "Blocks",
+              translations: {
+                es: "Bloques",
+                de: "Blöcke",
+              },
+              autogenerate: { directory: "guides/development/blocks" },
+            },
+            {
+              label: "Items",
+              translations: {
+                es: "Objetos",
+                de: "Items",
+              },
+              autogenerate: { directory: "guides/development/items" },
+            },
+            {
+              label: "Network",
+              translations: {
+                es: "Red",
+                de: "Netzwerk",
+              },
+              autogenerate: { directory: "guides/development/network" },
+            },
+            {
+              label: "Tools",
+              translations: {
+                es: "Herramientas",
+                de: "Werkzeuge",
+              },
+              autogenerate: { directory: "guides/development/tools" },
+            },
+          ],
+        },
+        {
+          label: "Reference",
+          translations: {
+            es: "Referencias",
+            de: "Referenz",
+          },
+          autogenerate: { directory: "reference" },
+        },
+      ],
+      editLink: {
+        baseUrl: "https://github.com/Steel-Foundation/SteelDocs/edit/main/",
       },
-    ],
-    editLink: {
-      baseUrl: "https://github.com/Steel-Foundation/SteelDocs/edit/main/",
-    },
-    lastUpdated: true,
-    defaultLocale: "root",
-    locales: {
-      // English docs in `src/content/docs/`
-      root: {
-        label: "English",
-        lang: "en",
+      lastUpdated: true,
+      defaultLocale: "root",
+      locales: {
+        // English docs in `src/content/docs/`
+        root: {
+          label: "English",
+          lang: "en",
+        },
+        // Sprich Deutsch, du Hurensohn :O) `src/content/docs/de/`
+        de: {
+          label: "Deutsch",
+          lang: "de",
+        },
+        // Documentacion en Español en `src/content/docs/es`
+        es: {
+          label: "Español",
+          lang: "es",
+        },
       },
-      // Sprich Deutsch, du Hurensohn :O) `src/content/docs/de/`
-      de: {
-        label: "Deutsch",
-        lang: "de",
-      },
-      // Documentacion en Español en `src/content/docs/es`
-      es: {
-        label: "Español",
-        lang: "es",
-      },
-    },
-  }), react()],
+    }),
+    react(),
+  ],
 
   vite: {
     plugins: [tailwindcss()],
