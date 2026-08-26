40094001a9638e40f063c6b431b4e40b7631b0a7
Melther <lordmelther@gmail.com>
2026-08-26 22:39:08 +0200

    Update dependencies and fixes

--- astro.config.ts
+++ astro.config.ts
@@ -108,7 +108,7 @@ export default defineConfig({
             es: "Primeros pasos",
             de: "Erste Schritte",
           },
-          autogenerate: { directory: "getting-started" },
+          items: [{autogenerate: { directory: "getting-started" }}],
         },
         {
           label: "Configuration",
@@ -116,11 +116,11 @@ export default defineConfig({
             es: "Configuración",
             de: "Konfiguration",
           },
-          autogenerate: { directory: "configuration" },
+          items: [{autogenerate: { directory: "configuration" }}],
         },
         {
           label: "Commands",
-          autogenerate: { directory: "commands" },
+          items: [{autogenerate: { directory: "commands" }}],
         },
         {
           label: "Development",
@@ -170,7 +170,7 @@ export default defineConfig({
             es: "Referencias",
             de: "Referenz",
           },
-          autogenerate: { directory: "reference" },
+          items: [{autogenerate: { directory: "reference" }}],
         },
       ],
       editLink: {
